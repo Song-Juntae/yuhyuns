0774c31e26aae91c0163f31c18abb8edbe268a7a
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-16 17:19:18 +0900

    .

--- pages/대시보드.py
+++ pages/대시보드.py
@@ -20,7 +20,13 @@ df = pd.read_csv('/app/yuhyuns/data/날짜별_가게별_카테모리매출합.cs
 # 시각화
 
 with col_1_0:
-    가게25번카테고리매출합 = pd.DataFrame(df.iloc[:,2:][df['store_nbr'] == 25].dropna().sum()).sort_values(0, ascending=False)
+    options = st.multiselect(
+        '가게 번호를 선택해주세요.',
+        [1,2,3,4,5,6,7,8,9,10],
+        ['Yellow', 'Red'])
+    
+    st.write('가게 번호:', options)
+    가게25번카테고리매출합 = pd.DataFrame(df.iloc[:,2:][df['store_nbr'] == options].dropna().sum()).sort_values(0, ascending=False)
     가게25번카테고리매출합 = 가게25번카테고리매출합.reset_index()
     가게25번카테고리매출합.columns = ['카테고리','매출합']
 
