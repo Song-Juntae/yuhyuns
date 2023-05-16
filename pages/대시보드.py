f176cb57d968142845bbe31cae6675bb2f7d0813
Song-Juntae <123853844+Song-Juntae@users.noreply.github.com>
2023-05-16 17:36:15 +0900

    .

--- pages/대시보드.py
+++ pages/대시보드.py
@@ -24,22 +24,21 @@ df = pd.read_csv('/app/yuhyuns/data/날짜별_가게별_카테모리매출합.cs
 with col_0_1:
     options = st.selectbox(
         '가게 번호를 선택해주세요.',
-        (1,2,3,4,5,6,7,8,9,10))
+        (1,2,3,4,5,6,7,8,9,10), key='line_options')
+    st.write('가게 번호:', options)
     라인그래프 = df.set_index('date').iloc[:,1:][df.set_index('date')['store_nbr'] == options].sum(axis=1)
     라인그래프 = 라인그래프.reset_index()
     라인그래프.columns = ['date','매출합']
     fig = go.Figure([go.Line(x=라인그래프['date'], y=라인그래프['매출합'])])
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True, key='line')
 
 with col_1_1:
     options2 = st.selectbox(
         '가게 번호를 선택해주세요.',
-        (1,2,3,4,5,6,7,8,9,10))
-    
+        (1,2,3,4,5,6,7,8,9,10), key='bar_options2')
     st.write('가게 번호:', options2)
     가게25번카테고리매출합 = pd.DataFrame(df.iloc[:,2:][df['store_nbr'] == options2].dropna().sum()).sort_values(0, ascending=False)
     가게25번카테고리매출합 = 가게25번카테고리매출합.reset_index()
     가게25번카테고리매출합.columns = ['카테고리','매출합']
-
     fig = go.Figure([go.Bar(x=가게25번카테고리매출합['카테고리'], y=가게25번카테고리매출합['매출합'])])
-    st.plotly_chart(fig, use_container_width=True)
+    st.plotly_chart(fig, use_container_width=True, key='bar')
